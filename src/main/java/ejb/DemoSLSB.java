d5e39152e6fab6de4012d713f2b89f8225d1bf61
Ian Darwin <ian@darwinsys.com>
2015-08-05 10:08:32 -0700

    All three views work, use @LocalBean

--- src/main/java/ejb/DemoSLSB.java
+++ src/main/java/ejb/DemoSLSB.java
@@ -2,22 +2,22 @@ package ejb;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-import javax.ejb.Local;
+import javax.ejb.LocalBean;
 import javax.ejb.Remote;
 import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.inject.Named;
+import javax.jws.WebService;
 
 /**
- * At least on WildFly 8, if you have all 3 annotations on
- * a no-interface bean, it cannot be @EJB-injected into JSF,
- * but if you have only 2 (@Local and @Remote) it can. Go figure.
- * @author Ian
+ * For the no-interface view, you probably want @LocalBean
+ * instead of @Local if you have other views (Remote, WebService)
+ * @author Ian Darwin
  */
 @Stateless
-@Local
+@LocalBean
 @Remote
-// @WebService
+@WebService
 @Named
 public class DemoSLSB {
 	
